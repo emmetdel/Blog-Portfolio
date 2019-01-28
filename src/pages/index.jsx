66a675f3dc94ca5f48428e52b4a0986a4adb34aa
Emmet Delaney <emmetdel@gmail.com>
2019-01-28 21:36:50 +0000

    finished index page styles

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,10 +10,6 @@ const Wrapper = styled.section`
   justify-content: center;
   align-items: center;
   font-family: 'Montserrat';
-
-  h1 {
-    margin-bottom: 12.5px;
-  }
 `;
 
 const LinksList = styled.ul`
@@ -31,28 +27,61 @@ const LinkAnchor = styled.a`
   &:visited,
   &:link,
   &:active {
-    text-decoration: none;
     color: black;
+    border-bottom-width: 0;
   }
   &:hover {
-    background-color: yellow;
+    transition-timing-function: cubic-bezier(0.075, 0.82, 0.165, 1);
+    transition: 0.2s;
+    background-color: #e65100;
+    color: white;
+    scale: 2;
+    padding-right: 35px;
+    border-radius: 0 2px 10px 0;
+    font-style: italic;
   }
+`;
+
+const Title = styled.h1`
+  margin-top: 2.5px;
+  margin-bottom: 2px !important;
+`;
+const SubTitle = styled.h3`
+  margin-top: 0;
+  margin-bottom: 20px;
+  text-transform: lowercase;
+  font-size: 0.9rem;
+  color: #e65100;
 `;
 
 class BlogIndex extends React.Component {
   render() {
     const { data } = this.props;
     const siteTitle = data.site.siteMetadata.title;
-    const links = ['Curriculum Vitae', 'Portfolio', 'Blog'];
+    const links = [
+      {
+        Name: 'Curriculum Vitae',
+        Href: '/cv',
+      },
+      {
+        Name: 'Portfolio',
+        Href: '/portfolio',
+      },
+      {
+        Name: 'Blog',
+        Href: '/blog',
+      },
+    ];
     return (
       <Wrapper>
         <SEO title="All posts" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
         <div>
-          <h1>{siteTitle}</h1>
+          <Title>{siteTitle}</Title>
+          <SubTitle>Software / Web Developer</SubTitle>
           <LinksList>
             {links.map(link => (
-              <li>
-                <LinkAnchor href="asd">{link}</LinkAnchor>
+              <li key={link.Href}>
+                <LinkAnchor href={link.Href}>{link.Name}</LinkAnchor>
               </li>
             ))}
           </LinksList>
